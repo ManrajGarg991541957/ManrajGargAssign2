b4cfbc6afa8de906182986bff703613e400cdaa0
Manraj <manraj_g93@hotmail.com>
2021-02-25 20:27:23 -0500

    Initial commit

---
+++ ManrajAssign22/src/main/java/manraj/garg/s991541957/ManrajActivity.java
@@ -0,0 +1,17 @@
+package manraj.garg.s991541957;
+/*
+Manraj Garg s991541957
+This is assignment 2
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManrajActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
